f95f0c36af40ff0439b34818c6253e70663997c5
Anton <you@example.combelitskeya@gmail.com>
2019-03-25 23:42:01 +0200

    Create deactivate post

--- slando/slando/settings.py
+++ slando/slando/settings.py
@@ -150,9 +150,16 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ),
     'EXCEPTION_HANDLER': 'rest_framework.views.exception_handler',
-
-
 }
+
+BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = TIME_ZONE
+
+
 SWAGGER_SETTINGS = {
     'SECURITY_DEFINITIONS': {
         'api_key': {
